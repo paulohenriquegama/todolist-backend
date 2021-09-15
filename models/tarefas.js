27ef476b043bb8b96f0fb0e98a0b06254a913436
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-09-15 17:11:57 -0300

    tipo prioridade

--- models/tarefas.js
+++ models/tarefas.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const tarefaModel = new mongoose.Schema({
   titulo: {type: String, require: true},
   desc: {type: String},
-  prioridade: {type: Number, require: true},
+  prioridade: {type: String, require: true},
   status: {type: String, require: true},
   prazo: {type: Date, require: true},
   dataCriacao: {type: Date, default: Date.now}
